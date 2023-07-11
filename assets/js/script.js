52ab79186d4b9a879377aa9c5b925baf7e692f3e
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-07-11 10:18:13 -0400

    Cleaned up code and removed test logs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
 var saveButtons = $(".saveBtn");
 
@@ -9,12 +6,6 @@ var tasks = [];
 
 
 $(function () {
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
   // shows date and time
   setInterval(function() {
     // grab today's date
@@ -34,10 +25,6 @@ $(function () {
       $("#" + allTasks[i].time).children("textarea").text(allTasks[i].task);
     }
   }
-
-  // test
-  console.log("start: ");
-  console.log(allTasks);
 
   saveButtons.on("click", function (){
     // save to local storage as array with time saved
